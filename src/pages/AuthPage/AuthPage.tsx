3513e1c780f49058d97cc9c214248d5d894bad27
djole-onogee <subotic.djo@gmail.com>
2024-03-06 09:00:28 +0100

    change style text
    style text

--- src/pages/AuthPage/AuthPage.tsx
+++ src/pages/AuthPage/AuthPage.tsx
@@ -6,7 +6,7 @@ import {
   LogoIconWrap,
   PhoneNumberWrap,
   StyledPaper,
-} from "./AuthPageStyle";
+} from "./AuthStyle";
 import { TextField } from "@mui/material";
 import { useRouter } from "next/navigation";
 import PageTransition from "@/components/PageTransition/PageTransition";
