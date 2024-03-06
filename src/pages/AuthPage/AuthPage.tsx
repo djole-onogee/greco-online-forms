43031fb95203438113f30eaea4961ab51a63d8ec
djole-onogee <subotic.djo@gmail.com>
2024-03-06 09:07:20 +0100

    delete style folders
    delete style folder

--- src/pages/AuthPage/AuthPage.tsx
+++ src/pages/AuthPage/AuthPage.tsx
@@ -1,16 +1,63 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import {
-  AnimatedIcon,
-  BackgroundWrap,
-  LogoIconWrap,
-  PhoneNumberWrap,
-  StyledPaper,
-} from "./AuthStyle";
 import { TextField } from "@mui/material";
 import { useRouter } from "next/navigation";
 import PageTransition from "@/components/PageTransition/PageTransition";
 import Image from "next/image";
+import { Paper } from "@mui/material";
+import styled from "styled-components";
+import { keyframes } from "styled-components";
+import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
+
+const ringAnimation = keyframes`
+  0% { transform: rotate(0); }
+  10% { transform: rotate(15deg); }
+  20% { transform: rotate(-10deg); }
+  30% { transform: rotate(10deg); }
+  40% { transform: rotate(-5deg); }
+  50% { transform: rotate(5deg); }
+  60% { transform: rotate(0); }
+  100% { transform: rotate(0); }
+`;
+
+const AnimatedIcon = styled(LocalPhoneIcon)`
+  animation: ${ringAnimation} 2s ease-in-out infinite;
+  margin-right: 3px;
+`;
+
+const BackgroundWrap = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100dvh;
+  background-color: #fff;
+`;
+
+const StyledPaper = styled(Paper)`
+  border-radius: 12px;
+  box-shadow: 0px 0px 12px 0px #00000026;
+  min-height: 30dvh;
+  min-width: 30dvh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const LogoIconWrap = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+`;
+
+const PhoneNumberWrap = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  padding: 20px 20px 5px 20px;
+`;
 
 type Props = {};
 
--- src/pages/FormPage/FormPage.tsx
+++ src/pages/FormPage/FormPage.tsx
@@ -1,12 +1,5 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import {
-  AnimatedIcon,
-  BackgroundWrap,
-  LogoIconWrap,
-  PhoneNumberWrap,
-  StyledPaper,
-} from "./FormStyle";
 import { TextField } from "@mui/material";
 import { useRouter } from "next/navigation";
 import PageTransition from "@/components/PageTransition/PageTransition";
@@ -15,6 +8,63 @@ import { Controller, useFormContext } from "react-hook-form";
 import QuestionYesNo from "@/components/Questions/QuestionYesNo/QuestionYesNo";
 import { motion } from "framer-motion";
 import Image from "next/image";
+import { Paper } from "@mui/material";
+import styled from "styled-components";
+import { keyframes } from "styled-components";
+import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
+
+const ringAnimation = keyframes`
+  0% { transform: rotate(0); }
+  10% { transform: rotate(15deg); }
+  20% { transform: rotate(-10deg); }
+  30% { transform: rotate(10deg); }
+  40% { transform: rotate(-5deg); }
+  50% { transform: rotate(5deg); }
+  60% { transform: rotate(0); }
+  100% { transform: rotate(0); }
+`;
+
+const AnimatedIcon = styled(LocalPhoneIcon)`
+  animation: ${ringAnimation} 2s ease-in-out infinite;
+  margin-right: 3px;
+`;
+
+const BackgroundWrap = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  flex-direction: column;
+  align-items: center;
+  height: 100dvh;
+  background-color: #fff;
+  padding-top: 40px;
+`;
+
+const LogoIconWrap = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+  padding-bottom: 50px;
+`;
+
+const StyledPaper = styled(Paper)`
+  border-radius: 12px;
+  box-shadow: 0px 0px 12px 0px #00000026;
+  min-height: 30dvh;
+  min-width: 30dvh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const PhoneNumberWrap = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  padding: 20px 20px 10px 20px;
+`;
 
 type Props = {};
 
