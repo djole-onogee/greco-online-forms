b94d4ca3b88d04ae67e8c664c10ce41df928070e
djole-onogee <subotic.djo@gmail.com>
2024-03-23 23:59:56 +0100

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
 "use client";
 import AuthPage from "@/layouts/AuthPage/AuthPage";
 import { useSearchParams } from "next/navigation";
+import dynamic from "next/dynamic";
+
+const Auth = dynamic(() => import("@/layouts/AuthPage/AuthPage"), {
+  ssr: false,
+});
 export default function Home() {
   const searchParams = useSearchParams();
   const id = searchParams.get("id");
-  return <AuthPage id={id} />;
+  return <Auth id={id} />;
 }
