7562f518b7c9837f5bc3d08972516c9c9f190ab3
djole-onogee <subotic.djo@gmail.com>
2024-03-24 00:03:53 +0100

    aaa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 "use client";
 import AuthPage from "@/layouts/AuthPage/AuthPage";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 export default function Home() {
   const searchParams = useSearchParams();
   const id = searchParams.get("id");
-  return <AuthPage id={id} />;
+  return (
+    <Suspense>
+      <AuthPage id={id} />
+    </Suspense>
+  );
 }
